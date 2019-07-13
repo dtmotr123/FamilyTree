15ed7a0b95406ee7691f849a0232a1dbfc838c70
dtmotr123 <dymoran97@gmail.com>
2019-07-13 21:59:37 +0100

    DFS to find node working

--- src/Family.java
+++ src/Family.java
@@ -1,17 +1,47 @@
+import java.util.ArrayList;
 
 public class Family {
 	
 	//TODO variables 
-	
+	private Boolean name1IsMale; //if true, then name2 is female and vice-versa
+	private String name1;
+	private String name2;
+	private ArrayList<Family> children;
+//	private ArrayList<Boolean> childrenGenders = new ArrayList<Boolean>();
 	
 	public Family() 
 	{
-		//TODO create the family
+		name1IsMale = null;
+		name1 = null;
+		name2 = null;
+		children = new ArrayList<Family>();
 	}
 	
+	public Family(String childName) //Creates new child
+	{
+		name1 = childName;
+	}
+
 	public void setParentOf(String childName, String parentName)
 	{
-		//TODO setParent code
+		
+	}
+	
+	public Family findPersonRec(Family f, String nameSearch) //DFS using recursion to find family member
+	{
+		if(f.getName1().equalsIgnoreCase(nameSearch) || f.getName2().equalsIgnoreCase(nameSearch))
+		{
+			System.out.println("found and returning");
+			return f;
+		} else {
+			for(Family child : f.getChildren())
+			{
+				Family res = findPersonRec(child, nameSearch);
+				if(res != null)
+					return res;
+			}
+		}
+		return null;
 	}
 	
 	public void getChildrenOf(String parent)
@@ -37,6 +67,38 @@ public class Family {
 	public void isMale()
 	{
 		//TODO Check if male
+	}
+
+	public Boolean getName1IsMale() {
+		return name1IsMale;
+	}
+
+	public void setName1IsMale(Boolean name1IsMale) {
+		this.name1IsMale = name1IsMale;
+	}
+
+	public String getName1() {
+		return name1;
+	}
+
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+
+	public String getName2() {
+		return name2;
+	}
+
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+
+	public ArrayList<Family> getChildren() {
+		return children;
+	}
+
+	public void setChildren(ArrayList<Family> children) {
+		this.children = children;
 	}
 
 }
--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,14 @@ public class Test {
 	
 	public static void main(String[] args)
 	{
-		Family fam = new Family();
+		Family fam = new Family(); //root
+		fam.setName1("Bob");
+		fam.setName2("Sarah");
+		fam.getChildren().add(new Family());
+		fam.getChildren().get(0).setName1("Charlie");
+		fam.getChildren().get(0).setName2("Laura");
+		
+		Family ob = fam.findPersonRec(fam, "Bob");
+		System.out.println(ob.getName1());
 	}
 }
