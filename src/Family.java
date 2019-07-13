0fb250e22cc420096995ca5c7e367cb0dc0c7198
dtmotr123 <dymoran97@gmail.com>
2019-07-13 14:53:45 +0100

    Created initial methods and classes

---
+++ src/Family.java
@@ -0,0 +1,42 @@
+
+public class Family {
+	
+	//TODO variables 
+	
+	
+	public Family() 
+	{
+		//TODO create the family
+	}
+	
+	public void setParentOf(String childName, String parentName)
+	{
+		//TODO setParent code
+	}
+	
+	public void getChildrenOf(String parent)
+	{
+		//TODO getChildrenOf code
+	}
+	
+	public void female()
+	{
+		//TODO setToFemale code
+	}
+	
+	public void male()
+	{
+		//TODO setToMale code
+	}
+	
+	public void isFemale()
+	{
+		//TODO Check if female
+	}
+	
+	public void isMale()
+	{
+		//TODO Check if male
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+
+public class Test {
+
+	
+	public static void main(String[] args)
+	{
+		Family fam = new Family();
+	}
+}
